8616dc9b04d2273dc31a345cf266d6569325348b
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-06 18:07:09 -0500

    Initial Commit

---
+++ Day4iOSReadingPlistAndJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day4iOSReadingPlistAndJSON
+//
+//  Created by Nikita Sandhu on 2020-03-06.
+//  Copyright © 2020 Nikita Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

7ac9c8ba617bbbdcaceb1347dc9010374e07961a
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-06 18:24:18 -0500

    reading data from plist

--- Day4iOSReadingPlistAndJSON/ViewController.swift
+++ Day4iOSReadingPlistAndJSON/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        readPlistData()
         // Do any additional setup after loading the view.
     }
 
+    func readPlistData(){
+        if let bundlePath = Bundle.main.path(forResource: "Employee", ofType: "plist"){
+//            print(bundlePath)
+            if let dictionary = NSMutableDictionary(contentsOfFile: bundlePath){
+                print(dictionary)
+                if let countries = dictionary["countries"] as? [String]{
+                    for v in countries{
+                        print(v)
+                    }
+                }
+            }
+        }
+    }
 
 }
 
